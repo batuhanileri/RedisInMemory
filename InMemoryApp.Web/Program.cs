83fb0839edd6ab54b835f618e4518036a24bc158
batuhanileri <bileriofficial@gmail.com>
2025-01-04 00:54:54 +0300

    AddMemoryCache() services and IMemoryCache interface add

--- InMemoryApp.Web/Program.cs
+++ InMemoryApp.Web/Program.cs
@@ -2,6 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddMemoryCache();
+
+
 
 var app = builder.Build();
 
