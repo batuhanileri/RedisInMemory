998997e90f824ba73902b2904296863335c9af60
batuhanileri <bileriofficial@gmail.com>
2025-01-04 19:53:57 +0300

    AbsoluteExpiration and SlidingExpiration

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -14,16 +14,20 @@ namespace InMemoryApp.Web.Controllers
 
         public IActionResult Index()
         {
-            //1.yol
-            if(String.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
-            {
-                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
-            }
+            ////1.yol
+            //if(String.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
+            //{
+            //    _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            //}
             //2.yol
-            if(!_memoryCache.TryGetValue("zaman",out string zamancache))
-            {
-               _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
-            }
+            //if(!_memoryCache.TryGetValue("zaman",out string zamancache))
+            //{
+            MemoryCacheEntryOptions cacheOptions = new MemoryCacheEntryOptions();
+            cacheOptions.AbsoluteExpiration = DateTime.Now.AddSeconds(30); //ömrü 30 saniye
+            cacheOptions.SlidingExpiration = TimeSpan.FromSeconds(10); //Her 10 saniyede bir erişildiğinde 10 saniye daha uzar
+
+               _memoryCache.Set<string>("zaman", DateTime.Now.ToString(),cacheOptions);
+            //}
 
 
             return View();
@@ -31,10 +35,12 @@ namespace InMemoryApp.Web.Controllers
 
         public IActionResult Show()
         {
+            _memoryCache.TryGetValue("zaman", out string zamancache);
 
-            _memoryCache.Remove("zaman");
+            ViewBag.zaman = zamancache;
+            //_memoryCache.Remove("zaman");
 
-            ViewBag.zaman = _memoryCache.Get<string>("zaman");
+            //ViewBag.zaman = _memoryCache.Get<string>("zaman");
             return View();
         }
     }
